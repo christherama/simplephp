0e43a7f652b7a08631e0fd2a308ee80ab1c6a67f
Chris Ramacciotti <cjramey@gmail.com>
2015-09-19 14:31:17 -0500

    Moved 404 to pages directory

--- fragments/main.php
+++ fragments/main.php
@@ -12,6 +12,6 @@ $file = getcwd()."/pages/$page.php";
 if(file_exists($file)) {
 	include($file);
 } else { // File must not exist - give 404 error with page
-	include('fragments/404.php');
+	include('pages/404.php');
 }
 ?>
